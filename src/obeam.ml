78cf8bdbf4071a9f046b780dd77a5412649841f9
yutopp <yutopp@gmail.com>
2017-06-30 14:18:54 +0900

    setup a module structure

---
+++ src/obeam.ml
@@ -0,0 +1,19 @@
+module Chunk =
+  struct
+    include Chunk
+  end
+
+module Simple_term_format =
+  struct
+    include Simple_term_format
+  end
+
+module External_term_format =
+  struct
+    include External_term_format
+  end
+
+module Abstract_format =
+  struct
+    include Abstract_format
+  end
