d4974b8f2542d88cf14e89f33dc2bdba1bcfcdec
Yoshihiro Imai <yoshihiro503@proofcafe.org>
2019-02-27 18:50:27 +0900

    Add testing cases about record expressions

--- test/test_record.erl
+++ test/test_record.erl
@@ -1,7 +1,15 @@
 -module(test_record).
 
-%% test case for record decl
+%% test case for record
 -record(r, {a,
             b = 42,
             c :: string(),
             d = 57 :: integer()}).
+
+-export([f/0]).
+
+f() ->
+    R = #r{a = 3, c = "hello"}, % Record Creation
+    _ = R#r.c,                  % Record Field Access
+    Index = #r.b,               % Record Field Index
+    R#r{a = 100, c = "hoge"}.   % Record Update
--- test/test_record.ml
+++ test/test_record.ml
@@ -54,4 +54,83 @@ let%expect_test "test_record.beam" =
                       LitInteger
                       (line    7)
                       (integer 57))))))))))
+          (AttrExport (line 9) (function_arity_list ((f 0))))
+          (DeclFun
+            (line          11)
+            (function_name f)
+            (arity         0)
+            (clauses ((
+              ClsFun
+              (line 11)
+              (patterns       ())
+              (guard_sequence ())
+              (body (
+                ExprBody (
+                  exprs (
+                    (ExprMatch
+                      (line 12)
+                      (pattern (
+                        PatVar
+                        (line 12)
+                        (id   R)))
+                      (body (
+                        ExprRecord
+                        (line 12)
+                        (name r)
+                        (record_fields (
+                          (12 a (
+                            ExprLit (
+                              lit (
+                                LitInteger
+                                (line    12)
+                                (integer 3)))))
+                          (12 c (
+                            ExprLit (
+                              lit (
+                                LitString
+                                (line 12)
+                                (str  hello))))))))))
+                    (ExprMatch
+                      (line 13)
+                      (pattern (PatUniversal (line 13)))
+                      (body (
+                        ExprRecordFieldAccess
+                        (line 13)
+                        (expr (
+                          ExprVar
+                          (line 13)
+                          (id   R)))
+                        (name       r)
+                        (field_name c))))
+                    (ExprMatch
+                      (line 14)
+                      (pattern (
+                        PatVar
+                        (line 14)
+                        (id   Index)))
+                      (body (
+                        ExprRecordFieldIndex
+                        (line       14)
+                        (name       r)
+                        (field_name b))))
+                    (ExprRecordUpdate
+                      (line 15)
+                      (expr (
+                        ExprVar
+                        (line 15)
+                        (id   R)))
+                      (name r)
+                      (update_fields (
+                        (15 a (
+                          ExprLit (
+                            lit (
+                              LitInteger
+                              (line    15)
+                              (integer 100)))))
+                        (15 c (
+                          ExprLit (
+                            lit (
+                              LitString
+                              (line 15)
+                              (str  hoge))))))))))))))))
           FormEof)))) |}]
