1ff058d6626811b353a0094b35ff5e3cc411c0fc
Yoshihiro Imai <yoshihiro503@proofcafe.org>
2019-03-01 19:12:56 +0900

    add test case of float literal using exponential notation

--- test/test_float.ml
+++ test/test_float.ml
@@ -13,7 +13,11 @@ let%expect_test "test_float.beam" =
           (AttrMod
             (line        1)
             (module_name test_float))
-          (AttrExport (line 3) (function_arity_list ((f 0))))
+          (AttrExport
+            (line 3)
+            (function_arity_list (
+              (f 0)
+              (g 0))))
           (DeclFun
             (line          6)
             (function_name f)
@@ -31,4 +35,39 @@ let%expect_test "test_float.beam" =
                         LitFloat
                         (line  6)
                         (float 0.1))))))))))))
+          (DeclFun
+            (line          9)
+            (function_name g)
+            (arity         0)
+            (clauses ((
+              ClsFun
+              (line 9)
+              (patterns       ())
+              (guard_sequence ())
+              (body (
+                ExprBody (
+                  exprs (
+                    (ExprMatch
+                      (line 10)
+                      (pattern (PatUniversal (line 10)))
+                      (body (
+                        ExprLit (
+                          lit (
+                            LitFloat
+                            (line  10)
+                            (float 2300))))))
+                    (ExprMatch
+                      (line 11)
+                      (pattern (PatUniversal (line 11)))
+                      (body (
+                        ExprLit (
+                          lit (
+                            LitFloat
+                            (line  11)
+                            (float 0.0023))))))
+                    (ExprLit (
+                      lit (
+                        LitAtom
+                        (line 12)
+                        (atom ok))))))))))))
           FormEof)))) |}]
