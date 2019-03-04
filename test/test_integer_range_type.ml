c74f8cdc86f68cd4ce10d1a4734afc42327a28e7
Yoshihiro Imai <yoshihiro503@proofcafe.org>
2019-03-04 15:28:33 +0900

    Add a test case of an integer range type with binary operations

--- test/test_integer_range_type.ml
+++ test/test_integer_range_type.ml
@@ -33,4 +33,43 @@ let%expect_test "test_integer_range_type.beam" =
                     LitInteger
                     (line    6)
                     (integer 2))))))))
+          (DeclType
+            (line 9)
+            (name s)
+            (tvars ())
+            (ty (
+              TyRange
+              (line 9)
+              (low (
+                TyBinOp
+                (line 9)
+                (op   *)
+                (lhs (
+                  TyBinOp
+                  (line 9)
+                  (op   band)
+                  (lhs (
+                    TyLit (
+                      lit (
+                        LitInteger
+                        (line    9)
+                        (integer 1)))))
+                  (rhs (
+                    TyLit (
+                      lit (
+                        LitInteger
+                        (line    9)
+                        (integer 3)))))))
+                (rhs (
+                  TyLit (
+                    lit (
+                      LitInteger
+                      (line    9)
+                      (integer 5)))))))
+              (high (
+                TyLit (
+                  lit (
+                    LitChar
+                    (line  9)
+                    (uchar U+0061))))))))
           FormEof)))) |}]
